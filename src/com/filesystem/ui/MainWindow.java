f71476aa50b74d215e079efeb8b619cbae641426
Victor Arias <vicar176@gmail.com>
2016-06-12 21:51:22 -0600

    cambios

--- src/com/filesystem/ui/MainWindow.java
+++ src/com/filesystem/ui/MainWindow.java
@@ -20,7 +20,9 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import com.filesystem.function.CommandsExecute;
 import com.filesystem.model.VirtualDirectory;
 import com.filesystem.model.VirtualFile;
+
 import javax.swing.JScrollPane;
+
 import java.awt.Font;
 
 public class MainWindow {
@@ -31,11 +33,13 @@ public class MainWindow {
 	private JTextArea consola;
 	private CommandsExecute commandsExecute;
 	private JButton btnEjecutar;
+	private JButton btnGuardar;
 	private JLabel path;
 	private VirtualDirectory currentDir;
 	private JScrollPane scrollPane;
 	private JScrollPane scrollPane_1;
 	private JScrollPane scrollPane_2;
+	private String tempConsoleContent;
 
 	/**
 	 * Launch the application.
@@ -59,13 +63,15 @@ public class MainWindow {
 	}
 
 	public void executeCommand(String command, String instruccion) {
-		String [] values = null;
+		String[] values = null;
 		switch (command) {
 		case "CREATE":
 			values = instruccion.split(",");
-			try{
-				currentDir = commandsExecute.createVirtualDrive(Integer.parseInt(values[0]), Integer.parseInt(values[1]));
-				setConsoleText("Disco Virtual creado"); 
+			try {
+				currentDir = commandsExecute.createVirtualDrive(
+						Integer.parseInt(values[0]),
+						Integer.parseInt(values[1]));
+				setConsoleText("Disco Virtual creado");
 				setNodesToTree(currentDir.getNode());
 				path.setText(currentDir.getPath());
 				lineaComandos.setText("");
@@ -76,11 +82,13 @@ public class MainWindow {
 		case "FILE":
 			values = instruccion.split(",");
 			try {
-				String name = values [0];
-				String ext = values [1];
-				String content = values [2];
-				commandsExecute.createVirtualFile(content, name, ext, currentDir);
-				setNodesToTree((DefaultMutableTreeNode)currentDir.getNode().getRoot());
+				String name = values[0];
+				String ext = values[1];
+				String content = values[2];
+				commandsExecute.createVirtualFile(content, name, ext,
+						currentDir);
+				setNodesToTree((DefaultMutableTreeNode) currentDir.getNode()
+						.getRoot());
 				setConsoleText("Archivo creado");
 				path.setText(currentDir.getPath());
 				lineaComandos.setText("");
@@ -89,9 +97,11 @@ public class MainWindow {
 			}
 			break;
 		case "MKDIR":
-			try{
-				currentDir = commandsExecute.createVirtualDirectory(instruccion, currentDir);
-				setNodesToTree((DefaultMutableTreeNode)currentDir.getNode().getRoot());
+			try {
+				currentDir = commandsExecute.createVirtualDirectory(
+						instruccion, currentDir);
+				setNodesToTree((DefaultMutableTreeNode) currentDir.getNode()
+						.getRoot());
 				setConsoleText("Directorio creado");
 				path.setText(currentDir.getPath());
 				lineaComandos.setText("");
@@ -102,7 +112,7 @@ public class MainWindow {
 		case "CD":
 		case "CDIR":
 		case "CambiarDIR":
-			try{
+			try {
 				currentDir = commandsExecute.changeDirectory(instruccion);
 				path.setText(currentDir.getPath());
 				lineaComandos.setText("");
@@ -112,35 +122,39 @@ public class MainWindow {
 			break;
 		case "LDIR":
 		case "ListarDIR":
-			try{
+			try {
 				StringBuilder sb = new StringBuilder();
-				Map<String, List<String>> dirContentMap = commandsExecute.listDirContent(currentDir);
+				Map<String, List<String>> dirContentMap = commandsExecute
+						.listDirContent(currentDir);
 				List<String> files = dirContentMap.get("files");
 				List<String> directories = dirContentMap.get("directories");
-				if(files == null && directories == null){
+				if (files == null && directories == null) {
 					sb.append("Directorio vacio");
 				}
-				if(files != null && files.size() > 0){
-					for(String name : files){
+				if (files != null && files.size() > 0) {
+					for (String name : files) {
 						sb.append(name + "\n");
 					}
 				}
-				if(directories != null && directories.size() > 0){
-					for(String name : directories){
+				if (directories != null && directories.size() > 0) {
+					for (String name : directories) {
 						sb.append(name + "\n");
 					}
 				}
 				setConsoleText(sb.toString());
+				lineaComandos.setText("");
 			} catch (Exception e) {
 				setConsoleText(e.getMessage());
 			}
 			break;
 		case "MFILE":
 		case "ModFILE":
-			values = instruccion.split(",");
-			try{
-				String name = values [0];
-				String content = values [1];
+			try {
+				String content = commandsExecute.verContenido(instruccion, currentDir);
+				tempConsoleContent = consola.getText();
+				consola.setText(content);
+				consola.setEditable(true);
+				btnGuardar.setVisible(true);
 			} catch (Exception e) {
 				lineaComandos.setText("");
 				setConsoleText(e.getMessage());
@@ -148,8 +162,9 @@ public class MainWindow {
 			break;
 		case "VP":
 		case "VerPropiedades":
-			try{
-				String output = commandsExecute.verPropiedades(instruccion, currentDir);
+			try {
+				String output = commandsExecute.verPropiedades(instruccion,
+						currentDir);
 				setConsoleText(output);
 				lineaComandos.setText("");
 			} catch (Exception e) {
@@ -159,8 +174,9 @@ public class MainWindow {
 			break;
 		case "CF":
 		case "ContFile":
-			try{
-				String output = commandsExecute.verContenido(instruccion, currentDir);
+			try {
+				String output = commandsExecute.verContenido(instruccion,
+						currentDir);
 				setConsoleText(output);
 				lineaComandos.setText("");
 			} catch (Exception e) {
@@ -170,12 +186,14 @@ public class MainWindow {
 			break;
 		case "CP":
 		case "CoPY":
-			try{
-				//para copiar hay que estar en el directorio donde esta el archivo que se quiere copiar
-				//o directorio a donde se quiere copiar
-				String []paths = instruccion.split(","); 
+			try {
+				// para copiar hay que estar en el directorio donde esta el
+				// archivo que se quiere copiar
+				// o directorio a donde se quiere copiar
+				String[] paths = instruccion.split(",");
 				commandsExecute.copy(currentDir, paths[0], paths[1]);
-				setNodesToTree((DefaultMutableTreeNode)currentDir.getNode().getRoot());
+				setNodesToTree((DefaultMutableTreeNode) currentDir.getNode()
+						.getRoot());
 				lineaComandos.setText("");
 			} catch (Exception e) {
 				lineaComandos.setText("");
@@ -185,11 +203,12 @@ public class MainWindow {
 		case "MV":
 		case "MoVer":
 			values = instruccion.split(",");
-			try{
-				String from = values [0];
-				String to = values [1];
+			try {
+				String from = values[0];
+				String to = values[1];
 				commandsExecute.mover(currentDir, from, to);
-				setNodesToTree((DefaultMutableTreeNode)currentDir.getNode().getRoot());
+				setNodesToTree((DefaultMutableTreeNode) currentDir.getNode()
+						.getRoot());
 				lineaComandos.setText("");
 			} catch (Exception e) {
 				lineaComandos.setText("");
@@ -198,9 +217,10 @@ public class MainWindow {
 			break;
 		case "RM":
 		case "ReMove":
-			try{
+			try {
 				commandsExecute.remove(currentDir, instruccion);
-				setNodesToTree((DefaultMutableTreeNode)currentDir.getNode().getRoot());
+				setNodesToTree((DefaultMutableTreeNode) currentDir.getNode()
+						.getRoot());
 				lineaComandos.setText("");
 			} catch (Exception e) {
 				lineaComandos.setText("");
@@ -208,11 +228,11 @@ public class MainWindow {
 			}
 			break;
 		case "FIND":
-			try{
+			try {
 				StringBuilder sb = new StringBuilder();
 				List<String> resultados = commandsExecute.find(instruccion);
-				if(resultados != null) {
-					for(String res : resultados){
+				if (resultados != null) {
+					for (String res : resultados) {
 						sb.append(res + "\n");
 					}
 				}
@@ -224,7 +244,8 @@ public class MainWindow {
 			}
 			break;
 		case "clear":
-			setConsoleText("");
+			lineaComandos.setText("");
+			consola.setText("");
 			break;
 		default:
 			setConsoleText("El comando no existe o esta mal escrito");
@@ -237,20 +258,50 @@ public class MainWindow {
 		consoleOutput = consola.getText();
 		consola.setText(output + "\n" + consoleOutput);
 	}
-	
-	private void ejecutarComando(){
-		btnEjecutar.addActionListener(new ActionListener(){
+
+	private void ejecutarComando() {
+		btnEjecutar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				try{
-					String [] instruction = lineaComandos.getText().split(";");
+				try {
+					String[] instruction = lineaComandos.getText().split(";");
 					String command = instruction[0];
-					executeCommand(command, instruction[1]);
-				}catch(Exception ex){
+					switch (command) {
+					case "LDIR":
+					case "ListarDIR":
+					case "clear":
+						executeCommand(command, "");
+						break;
+					default:
+						executeCommand(command, instruction[1]);
+						break;
+					}
+				} catch (Exception ex) {
 					setConsoleText("Faltan parametros");
 				}
 			}
-			
+
+		});
+	}
+	
+	private void guardarContenido(){
+		btnGuardar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String fileName = lineaComandos.getText().split(";")[1];
+				String content = consola.getText();
+				try {
+					commandsExecute.actualizaContenido(fileName, currentDir, content);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+					String output = "No se pudo guardar el contenido en el archivo\n";
+					tempConsoleContent = output + tempConsoleContent;
+				}
+				consola.setText(tempConsoleContent);
+				consola.setEditable(false);
+				btnGuardar.setVisible(false);
+				lineaComandos.setText("");
+			}
 		});
 	}
 
@@ -260,55 +311,62 @@ public class MainWindow {
 
 	@SuppressWarnings({ "unchecked", "serial", "rawtypes" })
 	private void initialize() {
-		
+		tempConsoleContent = "";
 		commandsExecute = new CommandsExecute();
-		
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 858, 477);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+				btnGuardar = new JButton("Guardar");
+				btnGuardar.setFont(new Font("Tahoma", Font.PLAIN, 10));
+				btnGuardar.setBounds(607, 54, 71, 23);
+				btnGuardar.setVisible(false);
+				frame.getContentPane().add(btnGuardar);
 
 		path = new JLabel("");
 		path.setToolTipText("Posici\u00F3n Actual");
-		path.setBounds(145, 58, 543, 14);
+		path.setBounds(133, 58, 545, 14);
 		frame.getContentPane().add(path);
 
 		btnEjecutar = new JButton("Ejecutar Linea");
 		btnEjecutar.setBounds(10, 9, 126, 23);
 		frame.getContentPane().add(btnEjecutar);
-		
+
 		JLabel lblPathActual = new JLabel("Path actual :");
 		lblPathActual.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblPathActual.setBounds(54, 58, 81, 14);
 		frame.getContentPane().add(lblPathActual);
-				
-				scrollPane_1 = new JScrollPane();
-				scrollPane_1.setBounds(698, 9, 134, 419);
-				frame.getContentPane().add(scrollPane_1);
-		
-				tree = new JTree();
-				scrollPane_1.setViewportView(tree);
-				tree.setModel(new DefaultTreeModel(new DefaultMutableTreeNode("----") {
-					{
-					}
-				}));
-				tree.setAutoscrolls(true);
-				tree.setExpandsSelectedPaths(true);
-						
-						scrollPane_2 = new JScrollPane();
-						scrollPane_2.setBounds(146, 9, 532, 38);
-						frame.getContentPane().add(scrollPane_2);
-				
-						lineaComandos = new JTextArea();
-						scrollPane_2.setViewportView(lineaComandos);
-								
-								scrollPane = new JScrollPane();
-								scrollPane.setBounds(23, 83, 655, 345);
-								frame.getContentPane().add(scrollPane);
-						
-								consola = new JTextArea();
-								scrollPane.setViewportView(consola);
-								consola.setEditable(false);
+
+		scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(698, 9, 134, 419);
+		frame.getContentPane().add(scrollPane_1);
+
+		tree = new JTree();
+		scrollPane_1.setViewportView(tree);
+		tree.setModel(new DefaultTreeModel(new DefaultMutableTreeNode("----") {
+			{
+			}
+		}));
+		tree.setAutoscrolls(true);
+		tree.setExpandsSelectedPaths(true);
+
+		scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(146, 9, 532, 38);
+		frame.getContentPane().add(scrollPane_2);
+
+		lineaComandos = new JTextArea();
+		scrollPane_2.setViewportView(lineaComandos);
+
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(23, 83, 655, 345);
+		frame.getContentPane().add(scrollPane);
+
+		consola = new JTextArea();
+		scrollPane.setViewportView(consola);
+		consola.setEditable(false);
 		ejecutarComando();
+		guardarContenido();
 	}
 }
